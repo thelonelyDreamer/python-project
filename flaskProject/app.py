ee7c02750fe5d8fbe343923d90a52c8adcd328d6
cleboy <1192136710@qq.com>
2022-09-13 19:22:58 +0800

    auto

--- flaskProject/app.py
+++ flaskProject/app.py
@@ -7,6 +7,11 @@ app = Flask(__name__)
 def hello_world():  # put application's code here
     return 'Hello World!'
 
+@app.route('/{id}')
+def hi(id):
+    print(id)
+    return "hi"
+
 
 if __name__ == '__main__':
     app.run()
